2492ab6723b56c699f783730a6d9e8de52b55583
Param Birje <parambirje@gmail.com>
2024-07-13 16:49:44 +0530

    Added right-side Timeline section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me</Button>
+    <main className="max-w-[80vw] mx-auto h-screen flex gap-10 py-14 px-5 box-border">
+      <section id="home-left" className="w-1/2"></section>
+
+      <section
+        id="home-right"
+        className="overflow-y-auto overflow-x-hidden w-1/2 no-scrollbar"
+      >
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl uppercase">
+          Timeline
+        </h1>
+
+        <div
+          id="timeline-container"
+          className="grid grid-cols-2 auto-rows-[25vh] gap-5 min-h-3/4 mt-10 pr-1"
+        >
+          <div className="bg-muted rounded-md"></div>
+          <div className="bg-muted rounded-md"></div>
+          <div className="bg-muted rounded-md"></div>
+        </div>
+      </section>
     </main>
   );
 }
