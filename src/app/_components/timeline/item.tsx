1e422bb06e2da0f9d60251829cf90c40f3f6e7d6
ParamBirje <parambirje@gmail.com>
2024-07-13 20:08:46 +0530

    Minified timeline items heading

--- src/app/_components/timeline/item.tsx
+++ src/app/_components/timeline/item.tsx
@@ -4,7 +4,7 @@ export default function TimelineItem() {
   return (
     <div className="cursor-pointer bg-muted rounded-md bg-[url('https://github.com/shadcn.png')] bg-center bg-no-repeat bg-cover relative hover:scale-95 duration-200">
       <div className="absolute w-full h-full bg-gradient-to-t from-black to-transparent flex flex-col justify-end p-5">
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        <h4 className="scroll-m-20 text-lg font-semibold tracking-tight">
           Some title that goes here
         </h4>
       </div>
