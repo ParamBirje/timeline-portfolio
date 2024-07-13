70437b644310f37937a3ac333888ec5cbd49c873
ParamBirje <parambirje@gmail.com>
2024-07-13 19:52:31 +0530

    Added a hover effect to timeline items

--- src/app/_components/timeline/item.tsx
+++ src/app/_components/timeline/item.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TimelineItem() {
   return (
-    <div className="bg-muted rounded-md bg-[url('https://github.com/shadcn.png')] bg-center bg-no-repeat bg-cover relative">
+    <div className="cursor-pointer bg-muted rounded-md bg-[url('https://github.com/shadcn.png')] bg-center bg-no-repeat bg-cover relative hover:scale-95 duration-200">
       <div className="absolute w-full h-full bg-gradient-to-t from-black to-transparent flex flex-col justify-end p-5">
         <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
           Some title that goes here
