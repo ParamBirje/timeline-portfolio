74da90d9fffc6a278b633d1087fc398ca6ddca5f
ParamBirje <parambirje@gmail.com>
2024-07-13 19:50:53 +0530

    Styled timeline items with image and heading

--- src/app/_components/timeline/item.tsx
+++ src/app/_components/timeline/item.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function TimelineItem() {
-  return <div className="bg-muted rounded-md"></div>;
+  return (
+    <div className="bg-muted rounded-md bg-[url('https://github.com/shadcn.png')] bg-center bg-no-repeat bg-cover relative">
+      <div className="absolute w-full h-full bg-gradient-to-t from-black to-transparent flex flex-col justify-end p-5">
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+          Some title that goes here
+        </h4>
+      </div>
+    </div>
+  );
 }
