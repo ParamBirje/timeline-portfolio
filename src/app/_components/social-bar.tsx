2e0261de11f16e8fc9e5a465ce5124d2cfbf88f6
ParamBirje <parambirje@gmail.com>
2024-07-14 10:34:41 +0530

    Added links to socialBar

--- src/app/_components/social-bar.tsx
+++ src/app/_components/social-bar.tsx
@@ -5,20 +5,44 @@ import {
   TwitterLogoIcon,
 } from "@radix-ui/react-icons";
 import { DevToIcon } from "@/components/ui/icons";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 function SocialBar() {
+  const socialLinks = [
+    {
+      href: "https://linkedin.com/in/parambirje",
+      icon: <LinkedInLogoIcon className="h-6 w-6" />,
+    },
+    {
+      href: "https://github.com/parambirje",
+      icon: <GitHubLogoIcon className="h-6 w-6" />,
+    },
+    {
+      href: "https://x.com/parambirje",
+      icon: <TwitterLogoIcon className="h-6 w-6" />,
+    },
+    {
+      href: "https://dev.to/parambirje",
+      icon: <DevToIcon className="h-7 w-7" />,
+    },
+  ];
+
   return (
     <div className="mt-2 flex gap-3">
       <div className="bg-muted p-4 rounded-md w-fit flex items-center gap-5">
-        <LinkedInLogoIcon className="h-6 w-6" />
-        <GitHubLogoIcon className="h-6 w-6" />
-        <TwitterLogoIcon className="h-6 w-6" />
-        <DevToIcon className="h-7 w-7" />
+        {socialLinks.map((link) => (
+          <Link key={link.href} target="_blank" href={link.href}>
+            {link.icon}
+          </Link>
+        ))}
       </div>
 
-      <div className="bg-muted p-4 rounded-md w-fit flex items-center">
-        <EnvelopeClosedIcon className="h-5 w-5" />
-      </div>
+      <Link href="mailto:param.birje@gmail.com">
+        <Button variant="secondary" className="h-full w-fit">
+          <EnvelopeClosedIcon className="h-5 w-5" />
+        </Button>
+      </Link>
     </div>
   );
 }
