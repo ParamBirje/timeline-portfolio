153f8472e607a0a9445fb57b84885188f664ed4f
ParamBirje <parambirje@gmail.com>
2024-07-14 10:17:04 +0530

    Decreased border radius of card component and featured item image

--- src/app/_components/featured/item.tsx
+++ src/app/_components/featured/item.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function FeaturedItem() {
   return (
     <Card className="flex gap-5 min-h-16 relative left-0 hover:left-2 duration-200">
-      <div className="w-[10vw] bg-[url('https://github.com/shadcn.png')] bg-cover bg-center rounded-s-xl"></div>
+      <div className="w-[10vw] bg-[url('https://github.com/shadcn.png')] bg-cover bg-center rounded-s-lg"></div>
 
       <h4 className="scroll-m-20 text-sm font-semibold tracking-tight p-1 w-full my-auto opacity-90">
         Some title that goes here Lorem ipsum dolor ipsum dolor ...
