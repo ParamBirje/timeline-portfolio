08f021669711ed5cf8bda962537eed07a82d5574
Param Birje <parambirje@gmail.com>
2024-07-18 19:03:57 +0530

    Integrated user-config with SocialBar

--- src/app/_components/social-bar.tsx
+++ src/app/_components/social-bar.tsx
@@ -7,23 +7,24 @@ import {
 import { DevToIcon } from "@/components/ui/icons";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { UserProfile as user } from "@/lib/user-config";
 
 function SocialBar() {
   const socialLinks = [
     {
-      href: "https://linkedin.com/in/parambirje",
+      href: user.linkedin,
       icon: <LinkedInLogoIcon className="h-6 w-6 hover:scale-105" />,
     },
     {
-      href: "https://github.com/parambirje",
+      href: user.github,
       icon: <GitHubLogoIcon className="h-6 w-6 hover:scale-105" />,
     },
     {
-      href: "https://x.com/parambirje",
+      href: user.twitter,
       icon: <TwitterLogoIcon className="h-6 w-6 hover:scale-105" />,
     },
     {
-      href: "https://dev.to/parambirje",
+      href: user.devto,
       icon: <DevToIcon className="h-7 w-7 hover:scale-105" />,
     },
   ];
@@ -38,7 +39,7 @@ function SocialBar() {
         ))}
       </div>
 
-      <Link href="mailto:param.birje@gmail.com">
+      <Link href={`mailto:${user.email}`}>
         <Button variant="secondary" className="h-full w-fit">
           <EnvelopeClosedIcon className="h-5 w-5" />
         </Button>
