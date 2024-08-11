2f71f01cb7b4cd20d533b800969cb185047d4913
Param Birje <parambirje@gmail.com>
2024-08-11 15:14:07 +0530

    Added aceternity tracing-beam on /blog/slug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,11 +2,12 @@ import BlurFade from "@/components/magicui/blur-fade";
 import PrismLoader from "@/components/prism-loader";
 import NewsletterSubscribe from "@/components/subscribe";
 import { Button } from "@/components/ui/button";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 import { getClient } from "@/lib/apollo-client";
 import gqlQuery from "@/lib/gql-queries";
 import { HashnodePost } from "@/lib/types";
 import { format } from "date-fns";
-import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
+import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 
 const BLUR_FADE_DELAY = 0.04;
@@ -26,55 +27,57 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   return (
     <main>
-      <section id="blog">
-        <div className="space-y-12 w-full pb-12">
-          <BlurFade delay={BLUR_FADE_DELAY}>
-            <Link href="/blog">
-              <Button variant="ghost">
-                <ArrowLeftIcon className="h-4 mr-2" />
-                back to blog
-              </Button>
-            </Link>
-          </BlurFade>
-          <BlurFade delay={BLUR_FADE_DELAY * 1.5}>
-            <img
-              className="bg-gray-400 h-[35vh] w-full object-cover rounded-md"
-              src={post.coverImage.url}
-              alt="post-image"
-            />
-          </BlurFade>
-          <BlurFade delay={BLUR_FADE_DELAY * 2.5}>
-            <div className="flex flex-col justify-center space-y-4">
-              <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  {post.title}
-                </h2>
-                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  {format(new Date(post.publishedAt), "dd MMMM, yyyy")}
-                </p>
+      <TracingBeam className="px-4">
+        <section id="blog">
+          <div className="space-y-12 w-full pb-12">
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Link href="/blog">
+                <Button variant="ghost">
+                  <ArrowLeftIcon className="h-4 mr-2" />
+                  back to blog
+                </Button>
+              </Link>
+            </BlurFade>
+            <BlurFade delay={BLUR_FADE_DELAY * 1.5}>
+              <img
+                className="bg-gray-400 h-[35vh] w-full object-cover rounded-md"
+                src={post.coverImage.url}
+                alt="post-image"
+              />
+            </BlurFade>
+            <BlurFade delay={BLUR_FADE_DELAY * 2.5}>
+              <div className="flex flex-col justify-center space-y-4">
+                <div className="space-y-2">
+                  <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                    {post.title}
+                  </h2>
+                  <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                    {format(new Date(post.publishedAt), "dd MMMM, yyyy")}
+                  </p>
+                </div>
               </div>
-            </div>
-          </BlurFade>
-          <BlurFade delay={BLUR_FADE_DELAY * 3}>
-            <article
-              className="prose dark:prose-invert"
-              dangerouslySetInnerHTML={{ __html: content }}
-            ></article>
-            <PrismLoader />
-          </BlurFade>
-          <BlurFade delay={BLUR_FADE_DELAY * 3.5}>
-            <NewsletterSubscribe />
-          </BlurFade>
-          <BlurFade delay={BLUR_FADE_DELAY * 4}>
-            <Link href="/blog">
-              <Button variant="ghost">
-                <ArrowLeftIcon className="h-4 mr-2" />
-                back to blog
-              </Button>
-            </Link>
-          </BlurFade>
-        </div>
-      </section>
+            </BlurFade>
+            <BlurFade delay={BLUR_FADE_DELAY * 3}>
+              <article
+                className="prose dark:prose-invert"
+                dangerouslySetInnerHTML={{ __html: content }}
+              ></article>
+              <PrismLoader />
+            </BlurFade>
+            <BlurFade delay={BLUR_FADE_DELAY * 3.5}>
+              <NewsletterSubscribe />
+            </BlurFade>
+            <BlurFade delay={BLUR_FADE_DELAY * 4}>
+              <Link href="/blog">
+                <Button variant="ghost">
+                  <ArrowLeftIcon className="h-4 mr-2" />
+                  back to blog
+                </Button>
+              </Link>
+            </BlurFade>
+          </div>
+        </section>
+      </TracingBeam>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
 const config = {
   darkMode: ["class"],
@@ -78,7 +79,23 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    addVariablesForColors,
+  ],
 } satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config;
